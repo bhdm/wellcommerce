26912768acf6316a98e86e1525a591f460423340
Maxim <bhd.m@ya.ru>
2016-03-29 17:12:04 +0300

    first commit

---
+++ web/app.php
@@ -0,0 +1,11 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = require __DIR__ . '/../app/autoload.php';
+$kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
+$request  = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
